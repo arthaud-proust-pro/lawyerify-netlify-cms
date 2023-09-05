9c65c87023e83e118391b651db14cae05459cb1f
Arthaud <arthaudp33@gmail.com>
2023-09-05 11:48:08 +0200

    feat: working cms with refactored settings

--- admin/app/settings/available-collections.js
+++ admin/app/settings/available-collections.js
@@ -1,9 +1,17 @@
-export const availableCollections = [
-    'expertises',
-    'offices',
-    'persons',
-    'posts',
-    'categories',
-    'tags',
-    'publications'
-];
+import expertises from '../content/expertises.js'
+import offices from '../content/offices.js'
+import posts from '../content/posts.js'
+import categories from '../content/categories.js'
+import tags from '../content/tags.js'
+import publications from '../content/publications.js'
+import persons from '../content/persons.js'
+
+export default [
+    expertises,
+    offices,
+    persons,
+    posts,
+    categories,
+    tags,
+    publications,
+]
